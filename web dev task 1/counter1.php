2ca4227d052282228081d59671c35a58e91aac09
Prateek Wadhwani <prateek.w10@gmail.com>
2016-03-27 22:56:08 +0530

    Added files via upload

---
+++ web dev task 1/counter1.php
@@ -0,0 +1,10 @@
+<?php
+$count_my_page = ("maps.html");
+$hits = file($count_my_page);
+$hits[0] ++;
+$fp = fopen($count_my_page , "w");
+fputs($fp , "$hits[0]");
+fclose($fp);
+echo $hits[0];
+
+?>
